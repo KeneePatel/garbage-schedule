6eb7fad53c6dc859c039c1ea8fe8ca7b7fc21a6c
KeneePatel <patelkenee2804@gmail.com>
2024-08-23 16:31:03 -0400

    added password protection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 "use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { PasswordInput } from "@/components/password-input";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { toast } from "@/components/ui/use-toast";
 import supabase from "@/lib/supabase-client";
+import { compareSync, genSaltSync, hashSync } from "bcryptjs";
 import { Trash2Icon } from "lucide-react";
 import { KeyboardEvent, useEffect, useState } from "react";
 
@@ -14,6 +26,8 @@ type Roommate = {
 };
 
 export default function Home() {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [passwordConfirmed, setPasswordConfirmed] = useState<boolean>(false);
   const [roommates, setRoommates] = useState<Roommate[]>([]);
   const [turnIndex, setTurnIndex] = useState<number>(0);
 
@@ -92,52 +106,102 @@ export default function Home() {
     }
   };
 
-  return (
-    <div className="max-w-md mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Garbage Duty Roster</h1>
-      <ul className="space-y-2">
-        {roommates.map((roommate, index) => (
-          <li
-            key={index}
-            className={`flex items-center justify-between p-4 rounded-lg shadow-md ${
-              index === turnIndex
-                ? "bg-secondary-foreground text-secondary"
-                : "bg-background"
-            }`}
-          >
-            <div className="flex items-center gap-4">
-              <Avatar className="w-10 h-10 rounded-full text-primary">
-                <AvatarFallback>
-                  {roommate.name.charAt(0).toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-              <span className="font-medium">{roommate.name}</span>
-              {index === turnIndex && (
-                <Badge className="bg-green-600">On duty</Badge>
-              )}
-            </div>
-            <div className="flex items-center gap-2">
-              <Button
-                variant="outline"
-                size="sm"
-                onClick={() => removeRoommate(roommate.id)}
-              >
-                <Trash2Icon className="w-4 h-4 stroke-black" />
-              </Button>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <div className="flex flex-col my-2 gap-2">
-        <Button onClick={nextTurn}>Rotate</Button>
-        <div className="flex items-center gap-2">
-          <Input
-            type="text"
-            placeholder="Add a new roommate"
-            onKeyDown={handleKeyDown}
-          />
+  const onSubmit = () => {
+    const salt = genSaltSync(Number(process.env.NEXT_PUBLIC_PASSWORD_SALT));
+    const hash = hashSync(currentPassword, salt);
+    if (compareSync(currentPassword, process.env.NEXT_PUBLIC_PASSWORD_HASH!)) {
+      toast({
+        title: "Ayo",
+        description: "Welcome",
+      });
+      setPasswordConfirmed(true);
+    } else {
+      toast({
+        title: "Password Incorrect",
+        description: "Feel free to try again :)",
+      });
+      console.debug(`salt: ${salt}, hash: ${hash}`);
+    }
+  };
+
+  if (passwordConfirmed) {
+    return (
+      <div className="max-w-md p-4 w-full">
+        <h1 className="text-2xl font-bold mb-4">Garbage Duty Roster</h1>
+        <ul className="space-y-2">
+          {roommates.map((roommate, index) => (
+            <li
+              key={index}
+              className={`flex items-center justify-between p-4 rounded-lg shadow-md ${
+                index === turnIndex
+                  ? "bg-secondary-foreground text-secondary"
+                  : "bg-background"
+              }`}
+            >
+              <div className="flex items-center gap-4">
+                <Avatar className="w-10 h-10 rounded-full text-primary">
+                  <AvatarFallback>
+                    {roommate.name.charAt(0).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                <span className="font-medium">{roommate.name}</span>
+                {index === turnIndex && (
+                  <Badge className="bg-green-600">On duty</Badge>
+                )}
+              </div>
+              <div className="flex items-center gap-2">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  onClick={() => removeRoommate(roommate.id)}
+                >
+                  <Trash2Icon className="w-4 h-4 stroke-black" />
+                </Button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="flex flex-col my-2 gap-2">
+          <Button className="w-full" onClick={nextTurn}>
+            Rotate
+          </Button>
+          <div className="flex items-center gap-2">
+            <Input
+              type="text"
+              placeholder="Add a new roommate"
+              onKeyDown={handleKeyDown}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <Card className="max-w-md p-4">
+        <CardHeader>
+          <CardTitle>Password Required</CardTitle>
+          <CardDescription>
+            Password required to access the website due to some certain people
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <PasswordInput
+                value={currentPassword}
+                onChange={(e) => setCurrentPassword(e.target.value)}
+                placeholder="Enter Password..."
+              />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full" onClick={onSubmit}>
+            Enter
+          </Button>
+        </CardFooter>
+      </Card>
+    );
+  }
 }
